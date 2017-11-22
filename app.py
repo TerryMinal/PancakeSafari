15bdf7ca75bb23baa698b91c62bb99ff173bf21c
Irene Lam <ilam1@stuy.edu>
2017-11-22 12:58:06 -0500

    Templates for old and new convos created and linked to home

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, flash, Markup
+import urllib2, json, random, os
+from util import database
+
+app = Flask(__name__)
+app.secret_key = os.urandom(128)
+
+# root route // access and render NASA information
+@app.route("/")
+def home():
+    database.db_setup()
+    return render_template("home.html")
+
+@app.route("/new")
+def new():
+    return render_template("create_convo.html")
+
+@app.route("/old")
+def old():
+    return render_template("old_convo.html")
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
