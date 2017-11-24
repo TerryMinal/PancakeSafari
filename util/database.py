0d3bac6492a324e9e37cf510be0476907fda6914
Irene Lam <ilam1@stuy.edu>
2017-11-24 03:09:50 -0500

    fix merge conflicts

--- util/database.py
+++ util/database.py
@@ -14,7 +14,6 @@ def open_db():
     global db
     f = "database.db"
     db = sqlite3.connect(f, check_same_thread=False)  # open if f exists, otherwise create
-    # c = db.cursor()    #facilitate db ops
     return
 
 
@@ -25,27 +24,41 @@ def close():
     return
 
 
-def db_setup():
+def db_count():
+    global db
+    open_db()
+    c = db.cursor()
+    num = -2
+    try:
+        c.execute("SELECT count(*) FROM threads")
+        num = c.fetchall()
+    except:
+        print("Unable to get count")
+    close()
+    return num[0][0] + 1
+
+
+def db_setup(): # sets up initial database
     global db
     open_db()
     c = db.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS threads (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id INTEGER PRIMARY KEY,
         time DEFAULT CURRENT_TIMESTAMP NOT NULL,
         message_log TEXT NOT NULL,
         gif_url TEXT NOT NULL)''')
     close()
     return
 
-def create_thread(input_log, input_url):
+def create_thread(id, input_log, input_url): # creates a new row (thread) within the database
     global db
     try:
         open_db()
         c = db.cursor()
-        command = '''INSERT INTO threads (time, message_log, gif_url)
-            VALUES (date('now'), ?, ?)'''
+        command = '''INSERT INTO threads
+            VALUES (?, date('now'), ?, ?)'''
         # print command
-        c.execute(command, (input_log, input_url,))      # input_log and input_url are from user/db
+        c.execute(command, (id, input_log, input_url,))      # input_log and input_url are from user/db
         close()
     except:
         print "Error creating thread"
@@ -61,21 +74,56 @@ def get_thread(index):
         c.execute("SELECT message_log FROM threads WHERE id=?", (index,))      # input_log and input_url are from user/db
         rows = c.fetchall()
 
-        for i in rows:
-            print i
+        # for i in rows:
+        #     print i
 
         close()
     except:
         print "Error creating thread"
+
+    return rows[0][0]
+
+
+def update_thread(index, new_message):
+    global db
+    try:
+        curr_message = get_thread(index) # receives old thread
+        mess = curr_message[:-1] + " " + new_message[8:] # new thread trimmed and appended accordingly
+        print mess
+        open_db()
+        c = db.cursor()
+        c.execute("UPDATE threads SET message_log = ? WHERE id = ?", (mess, index,))
+        close()
+
+    except:
+        print "Error updating thread"
         return False
     return True
 
-db_setup()
-create_thread('"text":"Today was a happy day"',
-              "https://i.pinimg.com/736x/de/c5/d1/dec5d1d75fa4333d771e6fed6c97c958--dessin-ghibli-ghibli-watercolor.jpg")
-create_thread('"text":"Today was another happy day"',
-              "https://i.pinimg.com/736x/de/c5/d1/dec5d1d75fa4333d771e6fed6c97c958--dessin-ghibli-ghibli-watercolor.jpg")
-create_thread('"text":"I hope it continues to be like this"',
-              "https://i.pinimg.com/736x/de/c5/d1/dec5d1d75fa4333d771e6fed6c97c958--dessin-ghibli-ghibli-watercolor.jpg")
-get_thread(1)
-get_thread(3)
+
+def get_all_threads():
+    global db
+    try:
+        open_db()
+        c = db.cursor()
+        c.execute("SELECT * FROM threads")
+        rows = c.fetchall()
+        close()
+    except:
+        print "Error getting all threads"
+
+    return rows
+
+# db_setup()
+
+# Test cases
+# create_thread(1, '"text":"Hi"',
+#               "https://i.pinimg.com/736x/de/c5/d1/dec5d1d75fa4333d771e6fed6c97c958--dessin-ghibli-ghibli-watercolor.jpg")
+# create_thread(2, '"text":"Welcome"',
+#               "https://i.pinimg.com/736x/de/c5/d1/dec5d1d75fa4333d771e6fed6c97c958--dessin-ghibli-ghibli-watercolor.jpg")
+# create_thread(3, '"text":"Totoro"',
+#               "https://i.pinimg.com/736x/de/c5/d1/dec5d1d75fa4333d771e6fed6c97c958--dessin-ghibli-ghibli-watercolor.jpg")
+# get_thread(1)
+# update_thread(1,'"text":"New Updates"')
+# get_thread(1)
+# get_thread(3)
