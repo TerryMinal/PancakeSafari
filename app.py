03a4cf00040de944befa4ffbbc56527b0605ad7e
TerryMinal <tguan@stuy.edu>
2017-11-27 21:27:17 -0500

    moved cleberbot to app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-
 from flask import Flask, render_template, request, redirect, flash, Markup, url_for
 import urllib2, json, random, os
 from util import database, misc
 
 app = Flask(__name__)
 app.secret_key = os.urandom(128)
+
+API_KEYS = misc.get_keys()
+CLEVERBOT_KEY = API_KEYS["Cleverbot"]
+CLEVERBOT_BASE_URL = "https://www.cleverbot.com/getreply?key=" + CLEVERBOT_KEY
 
 # root route // access and render NASA information
 @app.route("/")
@@ -26,6 +29,32 @@ def insert_msg():
 @app.route("/old")
 def old():
     return render_template("old_convo.html", name=database.get_all_threads())
+
+@app.route("/create_conv")
+def create_conv():
+    c = requests.get(CLEVERBOT_BASE_URL)
+    j = c.json()
+    clever_output = j["output"]
+    clever_output = "cleverbot: " + clever_output + ","
+    cs = j["cs"] # cs is the conversation history parameter passed in the cleverbot url
+    conv_id = j["conversation_id"]
+    database.create_thread(conv_id, clever_output, "")
+    return redirect(url_for("conversation", conv_id = conv_id, cs = cs, clever_output = clever_output))
+
+# displays webpage
+@app.route("/conversation?conv_id=<conv_id>?cs=<cs>?initial_output=<clever_output>")
+def conversation(conv_id, cs, clever_output):
+    return render_template("convo.html", conv_id = conv_id, cs = cs, clever_output = clever_output)
+
+# responds to an ajax call that gives a user input and sends the outut of cleverbot
+@app.route("/clever_output")
+def clever_output():
+    clever_params = {"input": request.args.get("clever_input"), "conv_id": request.args.get("conv_id"), "cs":request.args.get("cs")}
+    c = requests.get(CLEVERBOT_BASE_URL, params=clever_params)
+    j = c.json()
+    clever_output = j["output"]
+    # database.update_thread(request.form["conv_id"], clever_output)
+    return jsonify(result=clever_output)
 
 if __name__ == "__main__":
     database.db_setup()
